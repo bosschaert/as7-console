f14e467a6735a8ec9d2a71c6c576b1e728d0ae9b
David Bosschaert <david.bosschaert@gmail.com>
2011-10-10 10:42:15 +0100

    Tweaks

--- gui/src/main/java/org/jboss/as/console/client/shared/subsys/ejb3/TimerServiceView.java
+++ gui/src/main/java/org/jboss/as/console/client/shared/subsys/ejb3/TimerServiceView.java
@@ -1,34 +1,37 @@
 package org.jboss.as.console.client.shared.subsys.ejb3;
 
+import java.util.EnumSet;
+
+import com.google.gwt.user.client.ui.Widget;
+
 import org.jboss.as.console.client.shared.dispatch.DispatchAsync;
 import org.jboss.as.console.client.shared.subsys.ejb3.model.StrictMaxBeanPool;
 import org.jboss.as.console.client.shared.subsys.ejb3.model.TimerService;
 import org.jboss.as.console.client.shared.viewframework.AbstractEntityView;
 import org.jboss.as.console.client.shared.viewframework.Columns;
 import org.jboss.as.console.client.shared.viewframework.EntityToDmrBridge;
+import org.jboss.as.console.client.shared.viewframework.FrameworkButton;
 import org.jboss.as.console.client.widgets.forms.FormMetaData;
 import org.jboss.as.console.client.widgets.forms.PropertyMetaData;
 import org.jboss.ballroom.client.widgets.forms.Form;
 import org.jboss.ballroom.client.widgets.forms.FormAdapter;
 import org.jboss.ballroom.client.widgets.tables.DefaultCellTable;
 
-public class TimerServiceView extends AbstractEntityView<TimerService>{
+public class TimerServiceView extends AbstractEntityView<TimerService> {
     private final FormMetaData formMetaData;
     private final EntityToDmrBridge<TimerService> bridge;
 
     public TimerServiceView(PropertyMetaData propertyMetaData, DispatchAsync dispatcher) {
-        super(TimerService.class);
+        super(TimerService.class, EnumSet.of(FrameworkButton.ADD));
         formMetaData = propertyMetaData.getBeanMetaData(TimerService.class).getFormMetaData();
         bridge = new SingleEntityToDmrBridgeImpl<TimerService>(propertyMetaData, TimerService.class, this, dispatcher);
     }
 
-    /*
     @Override
     public Widget createWidget() {
-        FormAdapter<TimerService> details = makeEditEntityDetailsForm();
-        return details.asWidget();
+        entityEditor = makeEntityEditor();
+        return entityEditor.asWidget();
     }
-    */
 
     @Override
     protected FormMetaData getFormMetaData() {
