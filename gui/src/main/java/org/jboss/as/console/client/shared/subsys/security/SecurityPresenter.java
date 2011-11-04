3879c5db696b59679d5e548e033287a654e7059a
David Bosschaert <david.bosschaert@gmail.com>
2011-11-04 13:23:44 +0000

    AS7-1335 Initial Security Presenter and View

--- gui/src/main/java/org/jboss/as/console/client/shared/subsys/security/SecurityPresenter.java
+++ gui/src/main/java/org/jboss/as/console/client/shared/subsys/security/SecurityPresenter.java
@@ -1,3 +1,21 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
 package org.jboss.as.console.client.shared.subsys.security;
 
 import com.google.gwt.event.shared.EventBus;
@@ -14,6 +32,9 @@ import org.jboss.as.console.client.shared.dispatch.DispatchAsync;
 import org.jboss.as.console.client.shared.subsys.RevealStrategy;
 import org.jboss.as.console.client.shared.viewframework.FrameworkView;
 
+/**
+ * @author David Bosschaert
+ */
 public class SecurityPresenter extends Presenter<SecurityPresenter.MyView, SecurityPresenter.MyProxy> {
     private final RevealStrategy revealStrategy;
 
@@ -31,6 +52,12 @@ public class SecurityPresenter extends Presenter<SecurityPresenter.MyView, Secur
         super(eventBus, view, proxy);
 
         this.revealStrategy = revealStrategy;
+    }
+
+    @Override
+    protected void onReset() {
+        super.onReset();
+        getView().initialLoad();
     }
 
     @Override
--- gui/src/main/java/org/jboss/as/console/client/shared/subsys/security/SecurityView.java
+++ gui/src/main/java/org/jboss/as/console/client/shared/subsys/security/SecurityView.java
@@ -1,3 +1,21 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
 package org.jboss.as.console.client.shared.subsys.security;
 
 import java.util.EnumSet;
@@ -19,12 +37,15 @@ import org.jboss.as.console.client.widgets.forms.ApplicationMetaData;
 import org.jboss.ballroom.client.widgets.forms.FormAdapter;
 import org.jboss.ballroom.client.widgets.tables.DefaultCellTable;
 
+/**
+ * @author David Bosschaert
+ */
 public class SecurityView extends AbstractEntityView<SecuritySubsystem> implements SecurityPresenter.MyView {
     private final SingleEntityToDmrBridgeImpl<SecuritySubsystem> bridge;
 
     @Inject
     public SecurityView(ApplicationMetaData propertyMetaData, DispatchAsync dispatcher) {
-        super(SecuritySubsystem.class, propertyMetaData, EnumSet.of(FrameworkButton.ADD, FrameworkButton.REMOVE));
+        super(SecuritySubsystem.class, propertyMetaData, EnumSet.allOf(FrameworkButton.class));
         bridge = new SingleEntityToDmrBridgeImpl<SecuritySubsystem>(propertyMetaData, SecuritySubsystem.class, this, dispatcher);
     }
 
